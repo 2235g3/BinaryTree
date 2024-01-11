d259c273f035f2205cedc2d6423d540bd4ea7271
jacob <cashinhand9033@gmail.com>
2024-01-11 18:08:42 +0000

    hhh

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,68 @@
+import java.util.Objects;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        binaryTree();
+    }
+
+    public static void binaryTree() {
+        int[] items = {1,2,3,4,5,6,7,8,10,11,12,13,14,15,16,17,18,19,20,21,22};
+        TreeNode sourceNode = new TreeNode(9, true);
+
+        TreeNode sortedTree = addItems(items, sourceNode);
+
+        InOrder(sortedTree);
+    }
+
+    public static TreeNode addItems(int[] items, TreeNode source) {
+        TreeNode current = source;
+        for (int item: items) {
+            current = recursAdd(current, item);
+        }
+
+        return current;
+    }
+
+    public static TreeNode recursAdd(TreeNode current, int item) {
+        TreeNode next;
+        TreeNode temp;
+
+        if (current == null) {
+            return new TreeNode(item, false);
+        }
+        else {
+            if (current.val > item) {
+                next = current.left;
+                temp = recursAdd(next, item);
+                current.left = temp;
+            }
+            else if (current.val < item) {
+                next = current.right;
+                temp = recursAdd(next, item);
+                current.right = temp;
+            }
+            else {
+                return current;
+            }
+        }
+        return current;
+    }
+
+    public static void InOrder(TreeNode sortedTree) {
+        if (sortedTree.left != null) {
+            InOrder(sortedTree.left);
+        }
+        else {
+            System.out.println(sortedTree.val);
+            sortedTree.traversed = true;
+        }
+
+        if (sortedTree.root && sortedTree.left != null && sortedTree.left.traversed) {
+            System.out.println(sortedTree.val);
+        }
+
+        if (sortedTree.right != null) {
+            InOrder(sortedTree.right);
+        }
     }
 }
--- src/TreeNode.java
+++ src/TreeNode.java
@@ -1,9 +1,22 @@
 
 public class TreeNode {
-    private int val;
-    private TreeNode[] ChildNodes;
+    int val;
+    TreeNode left;
+    TreeNode right;
+    boolean root;
+    boolean traversed;
 
-    public TreeNode(int val) {
+    TreeNode(int val, boolean root) {
         this.val = val;
+        this.root = root;
+        this.traversed = false;
+    }
+
+    private void setLeft(TreeNode left) {
+        this.left = left;
+    }
+
+    private void setRight(TreeNode right) {
+        this.right = right;
     }
 }
