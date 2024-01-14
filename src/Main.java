489055402631189ae52ecddd899d25c57b89d64e
jacob <cashinhand9033@gmail.com>
2024-01-14 22:26:22 +0000

    Amended In-Order and added Pre/Post-Order

--- src/Main.java
+++ src/Main.java
@@ -1,17 +1,21 @@
-import java.util.Objects;
-
 public class Main {
     public static void main(String[] args) {
         binaryTree();
     }
 
     public static void binaryTree() {
-        int[] items = {1,2,3,4,5,6,7,8,10,11,12,13,14,15,16,17,18,19,20,21,22};
+        int[] items = {4,3,2,1,5,7,6,10,8,11,12,16,17,20,13,14,18,19,22,21,15};
         TreeNode sourceNode = new TreeNode(9, true);
 
         TreeNode sortedTree = addItems(items, sourceNode);
+        TreeNode preSortedTree = sortedTree;
+        TreeNode postSortedTree = sortedTree;
 
         InOrder(sortedTree);
+        System.out.println();
+        PreOrder(preSortedTree);
+        System.out.println();
+        PostOrder(postSortedTree);
     }
 
     public static TreeNode addItems(int[] items, TreeNode source) {
@@ -51,6 +55,7 @@ public class Main {
     public static void InOrder(TreeNode sortedTree) {
         if (sortedTree.left != null) {
             InOrder(sortedTree.left);
+            System.out.println(sortedTree.val);
         }
         else {
             System.out.println(sortedTree.val);
@@ -63,6 +68,45 @@ public class Main {
 
         if (sortedTree.right != null) {
             InOrder(sortedTree.right);
+        }
+    }
+
+    public static void PreOrder(TreeNode sortedTree) {
+        if (sortedTree.left != null) {
+            System.out.println(sortedTree.val);
+            PreOrder(sortedTree.left);
+        }
+        else {
+            System.out.println(sortedTree.val);
+        }
+
+        if (sortedTree.right != null) {
+            PreOrder(sortedTree.right);
+        }
+    }
+
+    public static void PostOrder(TreeNode sortedNode) {
+        if (sortedNode.left != null) {
+            PostOrder(sortedNode.left);
+        }
+        if (sortedNode.right != null) {
+            PostOrder(sortedNode.right);
+        }
+        if (sortedNode.left == null && sortedNode.right == null) {
+            System.out.println(sortedNode.val);
+            sortedNode.traversed = true;
+        }
+        else if (sortedNode.right != null && sortedNode.right.traversed && sortedNode.left == null) {
+            System.out.println(sortedNode.val);
+            sortedNode.traversed = true;
+        }
+        else if (sortedNode.left != null && sortedNode.left.traversed && sortedNode.right == null) {
+            System.out.println(sortedNode.val);
+            sortedNode.traversed = true;
+        }
+        else if (sortedNode.left.traversed && sortedNode.right.traversed) {
+            System.out.println(sortedNode.val);
+            sortedNode.traversed = true;
         }
     }
 }
